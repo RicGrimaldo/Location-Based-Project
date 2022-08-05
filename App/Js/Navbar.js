b236363b00fae88b2875bdd498802ebc43d3fe16
RicGrimaldo <rgrimaldo2307@gmail.com>
2022-08-04 20:45:49 -0500

    Exit icon added

--- App/Js/Navbar.js
+++ App/Js/Navbar.js
@@ -3,5 +3,13 @@
     document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
         document.querySelector('.offcanvas-collapse').classList.toggle('open');
         document.getElementById('navbar').classList.toggle('open');
+        document.getElementById('toggleButton').classList.toggle('navbar-toggler-icon');
+        var toggleButton = document.getElementById('toggleButton');
+        if(toggleButton.classList.contains('navbar-toggler-icon')){
+            toggleButton.innerHTML = ``;
+        }
+        else{
+            toggleButton.innerHTML = `<img src="Assets/x-lg.svg" width="30" height="30">`;
+        }
     })
 })()
