39a2c86039fb3bbc2be1cb693e53371c0a5d84c2
RicGrimaldo <rgrimaldo2307@gmail.com>
2022-08-06 21:09:59 -0500

    App update
    Button for saving the location added, and modal for file upload added

--- App/Js/Navbar.js
+++ App/Js/Navbar.js
@@ -3,13 +3,23 @@
     document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
         document.querySelector('.offcanvas-collapse').classList.toggle('open');
         document.getElementById('navbar').classList.toggle('open');
-        document.getElementById('toggleButton').classList.toggle('navbar-toggler-icon');
-        var toggleButton = document.getElementById('toggleButton');
-        if(toggleButton.classList.contains('navbar-toggler-icon')){
-            toggleButton.innerHTML = ``;
+        document.getElementById('menu-icon').classList.toggle('toggle');
+        var menuIcon = document.getElementById('menu-icon');
+        if(menuIcon.classList.contains('toggle')){
+            menuIcon.src="Assets/menu-icon.svg";
         }
         else{
-            toggleButton.innerHTML = `<img src="Assets/x-lg.svg" width="30" height="30">`;
+            menuIcon.src = "Assets/x-lg.svg";
         }
     })
 })()
+
+document.addEventListener('DOMContentLoaded', function() {
+    navigator.geolocation.watchPosition((position) => {
+        document.getElementById('latitude').innerHTML = `Latitud: ` + position.coords.latitude.toFixed(7);
+        document.getElementById('longitude').innerHTML = `Longitud: ` + position.coords.longitude.toFixed(7);
+        // document.getElementById('texto1').innerHTML = document.getElementById('caja').getAttribute('gps-projected-entity-place');
+        document.getElementById('caja').setAttribute('gps-projected-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude};`)
+        // document.getElementById('texto1').innerHTML = document.getElementById('caja').getAttribute('gps-projected-entity-place');
+    });
+});
