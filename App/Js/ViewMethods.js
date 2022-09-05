95f47a4d1bb23f385e18eeb7ff25678e0d3172a1
RicGrimaldo <rgrimaldo2307@gmail.com>
2022-09-05 11:53:06 -0500

    Text visualization added

--- App/Js/ViewMethods.js
+++ App/Js/ViewMethods.js
@@ -1,12 +1,14 @@
 let ubications = [];
 
-function Ubication(id, tag, file_type, latitude, longitude, route_file) {
+// Ubication object
+function Ubication(id, tag, file_type, latitude, longitude, route_file, text) {
     this.id = id;
     this.tag = tag;
     this.file_type = file_type;
     this.latitude = latitude;
     this.longitude = longitude;
     this.route_file = route_file;
+    this.text = text;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -22,14 +24,14 @@ const paintCards = function(){
     let file;
     let scene = document.getElementById('scene');
     for(var i = 0; i < ubications.length; i++){
-        file = file_output(ubications[i]);
+        file = file_output(ubications[i], i);
         scene.innerHTML += file;
         alert(file);
     }
     $("#scene").contents().unwrap();
 }
 
-const file_output = function(ubication){
+const file_output = function(ubication, i){
     let file;
     let asset = document.querySelector('a-assets');
     switch(ubication.file_type){
@@ -62,8 +64,21 @@ const file_output = function(ubication){
                 smoothThreshold="5"
                 autoplay="false"
                 scale="1 1 1"
-                gps-entity-place="latitude: latitude: ${ubication.latitude}; longitude: ${ubication.longitude};"
+                gps-entity-place="latitude: ${ubication.latitude}; longitude: ${ubication.longitude};"
             ></a-video>`;
+            break;
+        case 'txt':
+            file = `<a-text
+            wrap-count="25"
+            width="auto"
+            value="${ubication.text}"
+            look-at="[gps-camera]"
+            align="center"
+            anchor="center"
+            baseline="center"
+            gps-entity-place="latitude:${ubication.latitude}; longitude: ${ubication.longitude}"
+            id="#${ubication.id}"
+            ></a-text>`;
             break;
     }
     console.log(file);
