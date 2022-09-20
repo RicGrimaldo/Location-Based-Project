f04c60e330fb771ce6d47356c1cac5c71090daf0
RicGrimaldo <rgrimaldo2307@gmail.com>
2022-09-19 21:29:42 -0500

    Allow the access to all methods of the HTTP header

--- App/PHP/ServerMethods.php
+++ App/PHP/ServerMethods.php
@@ -1,4 +1,6 @@
 <?php 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
 //  Depending of the method, is what will be done.
     if($_POST["action"] == "upload_file"){
         $filename = $_POST['filename'];
