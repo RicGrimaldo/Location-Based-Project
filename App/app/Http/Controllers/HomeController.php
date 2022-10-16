bd449dc553518ba31f0fae21a12aa0cf4a701989
RicGrimaldo <rgrimaldo2307@gmail.com>
2022-10-15 20:13:26 -0500

    Login and change password functions implemented

---
+++ App/app/Http/Controllers/HomeController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Hash;
+use Auth;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function showChangePasswordGet() {
+        return view('auth.passwords.change-password');
+    }
+
+    public function changePasswordPost(Request $request) {
+        if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
+            // The passwords matches
+            Alert::error('Error de contraseña', 'Tu antigua contraseña no coincide con la que has ingresado.');
+            return redirect()->back();
+        }
+
+        if(strcmp($request->get('current-password'), $request->get('new-password')) == 0){
+            // Current password and new password same
+            Alert::error('Error de contraseña', 'Tu nueva contraseña no puede ser la misma que la antigua contraseña.');
+            return redirect()->back();
+        }
+
+        $validatedData = $request->validate([
+            'current-password' => 'required',
+            'new-password' => 'required|string|min:8|confirmed',
+        ]);
+
+        //Change Password
+        $user = Auth::user();
+        $user->password = bcrypt($request->get('new-password'));
+        $user->save();
+
+        Alert::success('Hecho', 'Tu contraseña ha sido cambiada exitosamente.');
+
+        return redirect()->back();
+    }
+}
--- App/routes/web.php
+++ App/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/changePassword',[App\Http\Controllers\HomeController::class, 'showChangePasswordGet'])->name('changePasswordGet');
+    Route::post('/changePassword',[App\Http\Controllers\HomeController::class, 'changePasswordPost'])->name('changePasswordPost');
+});
