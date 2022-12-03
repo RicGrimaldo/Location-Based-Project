54403aac85b1c16a4294b9af5128884a9f3e3244
RicGrimaldo <rgrimaldo2307@gmail.com>
2022-12-03 15:31:55 -0600

    Upload file buttons fixed

--- App/resources/views/admin/files/create.blade.php
+++ App/resources/views/admin/files/create.blade.php
@@ -4,31 +4,17 @@
 <div class="container">
     <form action="{{ route('files.store') }}" enctype="multipart/form-data" method="post">
         @csrf
-        <div class="row">
-            <div class="col-8 offset-2">
-                <div class="row">
-                    <h1>Subir archivo</h1>
-                </div>
+        <h1>Subir archivo</h1>
+        <div class="d-grid gap-2">
+            <div class="d-grid gap-2">
+                <label for="file-upload" class="contenedor-btn-file">
+                    <i class="fas fa-file"></i> Seleccionar archivo
+                </label>
+                <input id="file-upload" type="file" style='display: none;' name="file-upload" enctype="multipart/form-data"/>
+                @error('file')
+                    <strong>{{ $message }}</strong>
+                @enderror
             </div>
-        </div>
-        <div class="row offset-2 d-flex justify-content-center">
-            <label for="file-upload" class="contenedor-btn-file">
-                <i class="fas fa-file"></i> Seleccionar archivo
-            </label>
-            <input id="file-upload" type="file" style='display: none;' name="file-upload" enctype="multipart/form-data"/>
-            <!-- <button class="contenedor-btn-file">
-                <i class="fas fa-file"></i>
-                Adjuntar archivo
-                <label for="btn-file"></label>
-                <input type="file" id="btn-file">
-            </button> -->
-
-            @error('file')
-                <strong>{{ $message }}</strong>
-            @enderror
-        </div>
-
-        <div class="row pt-4 offset-2">
             <button class="btn btn-primary" type="submit" id="btn-upload" disabled>Subir archivo</button>
         </div>
     </form>
