5d15b061639087903758212d8ee67961357f2719
RicGrimaldo <rgrimaldo2307@gmail.com>
2022-12-05 13:12:00 -0600

    Adding the ubications to the database

---
+++ App/database/migrations/2022_12_05_184611_create_ubications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ubications', function (Blueprint $table) {
+            $table->id();
+            $table->string('tag')->unique();
+            $table->string('file');
+            $table->string('file_type');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->text('text')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ubications');
+    }
+};
